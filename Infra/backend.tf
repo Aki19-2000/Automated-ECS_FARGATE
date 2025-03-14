0edec3cc1a7889323e577fdbc48ff1cb05f56a86
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-14 11:25:28 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "nsh-usecase1"
+    bucket         = "aki1920"
     key            = "env:/dev/uc3/terraform.tfstate"
-    region         = "us-west-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
